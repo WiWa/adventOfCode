b53fcd0be859d946c7c4204ffb8b1db889a756aa
Win Wang <wiwang@teslamotors.com>
2015-12-16 16:54:37 -0800

    super wip day 6.5

--- Day6a.hs
+++ Day6a.hs
@@ -1,23 +1,28 @@
--- TODO Maybe-ify?
+
 import System.Environment
 -- What is Data Structures
+import Data.Map.Lazy (Map)
 import qualified Data.Set as Set
 import Data.List.Split
 
--- Hello from Day3
--- | Point 2d
 type P2 = (Int, Int)
--- | Visited Graph 2d
 type VG2 = Set.Set P2
+-- | {x: {y: v} }
+type PtMap = Map Int (Map Int Int)
 
-through :: P2 -> P2 -> Set.Set P2
+through :: P2 -> P2 -> VG2
 through (a,b) (c,d) = foldr Set.insert Set.empty
                         [(x,y) | x <- [a..c], y <- [b..d]]
 
-parseCmd :: String -> VG2 -> VG2 -> VG2
-parseCmd "turn on" vg ps = vg `Set.union` ps
-parseCmd "turn off" vg ps = vg `Set.difference` ps
-parseCmd "toggle" vg ps = (vg `Set.difference` ps) `Set.union` (ps `Set.difference` vg)
+getPoint (a,b) = head [ p | p@(x,y,z) <- allPoints, (x,y) == (a,b) ]
+
+allPoints :: VG2
+allPoints = through (0,0) (999,999)
+
+parseCmd :: String -> VG2
+parseCmd "turn on" ps = vg `Set.union` ps
+parseCmd "turn off" ps = vg `Set.difference` ps
+parseCmd "toggle" ps = foldl (parseCmd "turn on") ps (replicate 2 0)
 
 commands = ["turn on", "turn off", "toggle"]
 
@@ -36,8 +41,4 @@ grokCmds :: [String] -> VG2
 grokCmds = foldl (flip makeCmd) Set.empty
 
 main = do
-    print "Day 6!"
-    args <- getArgs
-    info <- readFile $ head args
-    let ps = grokCmds $ lines info
-    print $ Set.size ps
+    print "Day 6a!"
